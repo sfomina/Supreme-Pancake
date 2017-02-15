722aa98638bf42afd40653d975caa3d2e1ee319e
sfomina <afomina@stuy.edu>
2017-02-15 01:34:03 -0500

    data produced

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -48,9 +48,13 @@ public class MergeSortTester
 	int[] test50 = new int [50];
 	double a = 0;
 	double b = 0;
+	double time1 = 0;
+	double time10 = 0;
+	double time50 = 0;
 	
 	System.out.println("~~~~~~~~~~ARRAY n=1~~~~~~~~~~~~~~"); 
 	int counter = 0;
+
 	while (counter < 9){
 	    //populate an array with one item
 	    test1[0] = (int) (Math.random() *10);
@@ -62,8 +66,63 @@ public class MergeSortTester
 	    MergeSort.sort(test1);
 	    b= System.currentTimeMillis();
 	    System.out.println("Time: " + (b - a));
+	    time1 += b-a;
 	    counter ++;
 	}
+	System.out.println("AVG TIME FOR n=1:" + (time1 *1.0 / 10));
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+	System.out.println("~~~~~~~~~~ARRAY n=10~~~~~~~~~~~~~~");
+	counter = 0;
+	while (counter<9){
+	    //populate a 10 item array
+	    int x = 0;
+	    while(x<10){
+		test10[x] = (int) (Math.random() * 50);
+		x++;
+	    }
+	    System.out.println("Testing on array ");
+	    MergeSort.printArray(test10);
+	    System.out.print(" ....\n");
+
+	    //start stopwatch
+	    a = System.currentTimeMillis();
+	    MergeSort.sort(test10);
+	    b= System.currentTimeMillis();
+
+	    System.out.println("Time: " + (b - a));
+	    time10 += b-a;
+	    counter ++;
+	}
+	System.out.println("AVG TIME FOR n=10:" + (time10 *1.0 / 10));
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+       	System.out.println("~~~~~~~~~~ARRAY n=50~~~~~~~~~~~~~~");
+	counter = 0;
+	while (counter<9){
+	    //populate a 10 item array
+	    int y = 0;
+	    while(y<50){
+		test50[y] = (int) (Math.random() * 50);
+		y++;
+	    }
+	    System.out.println("Testing on array ");
+	    MergeSort.printArray(test50);
+	    System.out.print(" ....\n");
+
+	    //start stopwatch
+	    a = System.currentTimeMillis();
+	    MergeSort.sort(test50);
+	    b= System.currentTimeMillis();
+
+	    System.out.println("Time: " + (b - a));
+	    time50 += b-a;
+	    counter ++;
+	}
+	System.out.println("AVG TIME FOR n=50:" + (time50 *1.0 / 10));
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+	
+	
 	/*
 	System.out.println("Standard nlogn time when n is 9");
 	System.out.println("Time merge sort takes to sort array of length 9");
