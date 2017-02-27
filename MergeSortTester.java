8b80099ebcbf97666ba698135fbd3d7da7b3dcfd
sfomina <afomina@stuy.edu>
2017-02-26 20:14:42 -0500

    revisions

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -41,150 +41,33 @@ public class MergeSortTester
     
     public static void main( String[] args ) 
     {
-	int[] test1= new int [1];
-	int[] test10 = new int [10];
-	int[] test5 = new int [5];
-	int[] test50 = new int [50];
-	int[] test100 = new int[100];
-	int[] test1000 = new int[1000];
+	int[] test1000= new int [1000];
+	int[] test1500 = new int [1500];
+	int[] test2000 = new int [2000];
+	int[] test5000 = new int [5000];
+	int[] test4000 = new int[4000];
+	int[] test3000 = new int[3000];
 	
 	double a = 0;
 	double b = 0;
-	double time1 = 0;
-	double time5 = 0;
-	double time10 = 0;
-	double time50 = 0;
-	double time100 = 0;
 	double time1000 = 0;
-	
-	System.out.println("~~~~~~~~~~ARRAY n=1~~~~~~~~~~~~~~"); 
-	int counter = 0;
-
-	while (counter < 9){
-	    //populate an array with one item
-	    test1[0] = (int) (Math.random() *10);
-	    System.out.println("Testing on array ");
-	    MergeSort.printArray(test1);
-	    System.out.print(" ....\n");
-	    //start stopwatch
-	    a = System.currentTimeMillis();
-	    MergeSort.sort(test1);
-	    b= System.currentTimeMillis();
-	    System.out.println("Time: " + (b - a));
-	    time1 += b-a;
-	    counter ++;
-	}
-
-	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-	System.out.println("~~~~~~~~~~ARRAY n=10~~~~~~~~~~~~~~");
-	counter = 0;
-	while (counter<9){
-	    //populate a 10 item array
-	    int x = 0;
-	    while(x<10){
-		test10[x] = (int) (Math.random() * 50);
-		x++;
-	    }
-	    System.out.println("Testing on array ");
-	    MergeSort.printArray(test10);
-	    System.out.print(" ....\n");
-
-	    //start stopwatch
-	    a = System.currentTimeMillis();
-	    MergeSort.sort(test10);
-	    b= System.currentTimeMillis();
-
-	    System.out.println("Time: " + (b - a));
-	    time10 += b-a;
-	    counter ++;
-	}
-
-	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-	System.out.println("~~~~~~~~~~ARRAY n=5~~~~~~~~~~~~~~");
-	counter = 0;
-	while (counter<9){
-	    //populate a 5 item array
-	    int p = 0;
-	    while(p<5){
-		test5[p] = (int) (Math.random() * 50);
-		p++;
-	    }
-	    System.out.println("Testing on array ");
-	    MergeSort.printArray(test5);
-	    System.out.print(" ....\n");
-
-	    //start stopwatch
-	    a = System.currentTimeMillis();
-	    MergeSort.sort(test5);
-	    b= System.currentTimeMillis();
-
-	    System.out.println("Time: " + (b - a));
-	    time5 += b-a;
-	    counter ++;
-	}
-
-	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-       	System.out.println("~~~~~~~~~~ARRAY n=50~~~~~~~~~~~~~~");
-	counter = 0;
-	while (counter<9){
-	    //populate a 50 item array
-	    int y = 0;
-	    while(y<50){
-		test50[y] = (int) (Math.random() * 50);
-		y++;
-	    }
-	    System.out.println("Testing on array ");
-	    MergeSort.printArray(test50);
-	    System.out.print(" ....\n");
-
-	    //start stopwatch
-	    a = System.currentTimeMillis();
-	    MergeSort.sort(test50);
-	    b= System.currentTimeMillis();
-
-	    System.out.println("Time: " + (b - a));
-	    time50 += b-a;
-	    counter ++;
-	}
-
-	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-	System.out.println("~~~~~~~~~~ARRAY n=100~~~~~~~~~~~~~~");
-	counter = 0;
-	while (counter<9){
-	    //populate a 100 item array
-	    int z = 0;
-	    while(z<100){
-		test100[z] = (int) (Math.random() * 100);
-		z++;
-	    }
-	    System.out.println("Testing on array ");
-	    MergeSort.printArray(test100);
-	    System.out.print(" ....\n");
-
-	    //start stopwatch
-	    a = System.currentTimeMillis();
-	    MergeSort.sort(test100);
-	    b= System.currentTimeMillis();
-
-	    System.out.println("Time: " + (b - a));
-	    time100 += b-a;
-	    counter ++;
-	}
+	double time5000 = 0;
+	double time2000 = 0;
+	double time1500 = 0;
+	double time3000 = 0;
+	double time4000 = 0;
+      
 
 	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
 
 	System.out.println("~~~~~~~~~~ARRAY n=1000~~~~~~~~~~~~~~");
-	counter = 0;
+	int counter = 0;
 	while (counter<9){
 	    //populate a 1000 item array
-	    int q = 0;
-	    while(q<1000){
-		test1000[q] = (int) (Math.random() * 1000);
-		q++;
+	    int x = 0;
+	    while(x<1000){
+		test1000[x] = (int) (Math.random() * 50);
+		x++;
 	    }
 	    System.out.println("Testing on array ");
 	    MergeSort.printArray(test1000);
@@ -201,13 +84,139 @@ public class MergeSortTester
 	}
 
 	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+	System.out.println("~~~~~~~~~~ARRAY n=5000~~~~~~~~~~~~~~");
+	counter = 0;
+	while (counter<9){
+	    //populate a 5000 item array
+	    int p = 0;
+	    while(p<5000){
+		test5000[p] = (int) (Math.random() * 50);
+		p++;
+	    }
+	    System.out.println("Testing on array ");
+	    MergeSort.printArray(test5000);
+	    System.out.print(" ....\n");
+
+	    //start stopwatch
+	    a = System.currentTimeMillis();
+	    MergeSort.sort(test5000);
+	    b= System.currentTimeMillis();
+
+	    System.out.println("Time: " + (b - a));
+	    time5000 += b-a;
+	    counter ++;
+	}
+
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+       	System.out.println("~~~~~~~~~~ARRAY n=1500~~~~~~~~~~~~~~");
+	counter = 0;
+	while (counter<9){
+	    //populate a 1500 item array
+	    int y = 0;
+	    while(y<1500){
+		test1500[y] = (int) (Math.random() * 50);
+		y++;
+	    }
+	    System.out.println("Testing on array ");
+	    MergeSort.printArray(test1500);
+	    System.out.print(" ....\n");
+
+	    //start stopwatch
+	    a = System.currentTimeMillis();
+	    MergeSort.sort(test1500);
+	    b= System.currentTimeMillis();
+
+	    System.out.println("Time: " + (b - a));
+	    time1500 += b-a;
+	    counter ++;
+	}
+
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+	System.out.println("~~~~~~~~~~ARRAY n=2000~~~~~~~~~~~~~~");
+	counter = 0;
+	while (counter<9){
+	    //populate a 2000 item array
+	    int z = 0;
+	    while(z<2000){
+		test2000[z] = (int) (Math.random() * 50);
+		z++;
+	    }
+	    System.out.println("Testing on array ");
+	    MergeSort.printArray(test2000);
+	    System.out.print(" ....\n");
+
+	    //start stopwatch
+	    a = System.currentTimeMillis();
+	    MergeSort.sort(test2000);
+	    b= System.currentTimeMillis();
+
+	    System.out.println("Time: " + (b - a));
+	    time2000 += b-a;
+	    counter ++;
+	}
+
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+	System.out.println("~~~~~~~~~~ARRAY n=3000~~~~~~~~~~~~~~");
+	counter = 0;
+	while (counter<9){
+	    //populate a 3000 item array
+	    int q = 0;
+	    while(q<3000){
+		test3000[q] = (int) (Math.random() * 1000);
+		q++;
+	    }
+	    System.out.println("Testing on array ");
+	    MergeSort.printArray(test3000);
+	    System.out.print(" ....\n");
+
+	    //start stopwatch
+	    a = System.currentTimeMillis();
+	    MergeSort.sort(test3000);
+	    b= System.currentTimeMillis();
+
+	    System.out.println("Time: " + (b - a));
+	    time3000 += b-a;
+	    counter ++;
+	}
+
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+		System.out.println("~~~~~~~~~~ARRAY n=4000~~~~~~~~~~~~~~");
+	counter = 0;
+	while (counter<9){
+	    //populate a 4000 item array
+	    int q = 0;
+	    while(q<4000){
+		test4000[q] = (int) (Math.random() * 1000000);
+		q++;
+	    }
+	    System.out.println("Testing on array ");
+	    MergeSort.printArray(test4000);
+	    System.out.print(" ....\n");
+
+	    //start stopwatch
+	    a = System.currentTimeMillis();
+	    MergeSort.sort(test4000);
+	    b= System.currentTimeMillis();
+
+	    System.out.println("Time: " + (b - a));
+	    time4000 += b-a;
+	    counter ++;
+	}
+
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
 	
-	System.out.println("AVG TIME FOR n=1:" + (time1 *1.0 / 10));
-	System.out.println("AVG TIME FOR n=5:" + (time5 *1.0 / 10));		
-	System.out.println("AVG TIME FOR n=10:" + (time10 *1.0 / 10));
-	System.out.println("AVG TIME FOR n=50:" + (time50 *1.0 / 10));
-	System.out.println("AVG TIME FOR n=100:" + (time100 *1.0 / 10));
-	System.out.println("AVG TIME FOR n=1000:" + (time1000 *1.0 / 10));		
+	System.out.println("AVG TIME FOR n=1000:" + (time1000 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=5000:" + (time5000 *1.0 / 10));		
+	System.out.println("AVG TIME FOR n=2000:" + (time2000 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=1500:" + (time1500 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=3000:" + (time3000 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=4000:" + (time4000 *1.0 / 10));		
 	/*
 	System.out.println("Standard nlogn time when n is 9");
 	System.out.println("Time merge sort takes to sort array of length 9");
